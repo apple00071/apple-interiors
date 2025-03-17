cd8dfc6318f92e7058370504269bad6593d15253
apple00071 <pavankumarv1497@gmail.com>
2025-03-17 20:48:44 +0530

    Fix Vercel deployment with static rendering config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+export const dynamic = 'force-static';
+
 export default function Home() {
   return (
     <main className="min-h-screen">
