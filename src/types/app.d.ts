6da55f1b7c1b29f59464b96c583ee8e70f616f89
apple00071 <pavankumarv1947@gmail.com>
2025-06-12 21:52:55 +0530

    Rebuild page

---
+++ src/types/app.d.ts
@@ -0,0 +1,18 @@
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      div: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+      nav: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+      header: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+      button: DetailedHTMLProps<HTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+      span: DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+      a: DetailedHTMLProps<HTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>
+      p: DetailedHTMLProps<HTMLAttributes<HTMLParagraphElement>, HTMLParagraphElement>
+      main: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>
+      svg: DetailedHTMLProps<HTMLAttributes<SVGElement>, SVGElement>
+      path: DetailedHTMLProps<HTMLAttributes<SVGPathElement>, SVGPathElement>
+    }
+  }
+} 
