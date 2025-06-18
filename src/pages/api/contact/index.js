e266b246414db2e3ced386345aa20a522c9cda02
apple00071 <pavankumarv1947@gmail.com>
2025-06-19 00:53:00 +0530

    uy

--- src/pages/api/contact/index.js
+++
@@ -1,6 +0,0 @@
-// This file handles requests to /api/contact/ (with trailing slash)
-// It simply re-exports the handler from /api/contact.js
-
-import contactHandler from '../contact';
-
-export default contactHandler; 
